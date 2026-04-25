a16892f9f96974e38d179cfafb19203d25753197
TanishaSathe <tanishaasathe@gmail.com>
2026-04-25 19:30:21 +0530

    script.js

--- clothes/script.js
+++ clothes/script.js
@@ -363,11 +363,11 @@ function proceedToPayment() {
 const products = [
     {
         id: 1,
-        name: "Summer Dress",
+        name: "Wool Sweater",
         price: 3499,
         image: "https://images.unsplash.com/photo-1434389677669-e08b4cac3105?w=400",
         category: "women",
-        description: "Elegant summer dress"
+        description: "Warm wool sweater"
     },
     {
         id: 2,
@@ -403,11 +403,11 @@ const products = [
     },
     {
         id: 6,
-        name: "Midi Skirt",
+        name: "Designer Top",
         price: 3999,
         image: "https://images.unsplash.com/photo-1594633312681-1dfe5d97dccd1?w=400",
         category: "women",
-        description: "Stylish midi skirt"
+        description: "Stylish designer top₹"
     },
     {
         id: 7,
